d9739655cfbb84c391a0f4e9bc96e129fb83fb17
Dade Sheng <dasheng@amazon.com>
2021-11-22 17:40:30 -0500

    Build a connect wallet button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  /*
-   * Just a state variable we use to store our user's public wallet.
-   */
   const [currentAccount, setCurrentAccount] = useState("");
 
   const checkIfWalletIsConnected = async () => {
@@ -18,9 +15,6 @@ const App = () => {
         console.log("We have the ethereum object", ethereum);
       }
 
-      /*
-       * Check if we're authorized to access the user's wallet
-       */
       const accounts = await ethereum.request({ method: "eth_accounts" });
 
       if (accounts.length !== 0) {
@@ -30,6 +24,29 @@ const App = () => {
       } else {
         console.log("No authorized account found");
       }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Implement your connectWallet method here
+   */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
@@ -52,6 +69,15 @@ const App = () => {
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
